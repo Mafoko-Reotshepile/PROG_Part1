6a586a28decaae80c38f066ef59aad8f612eb36b
Mafoko-Reotshepile <163289791+Mafoko-Reotshepile@users.noreply.github.com>
2025-04-13 08:21:10 +0000

    Added 3 classes and called them in the main class

---
+++ src/registrationloginappp/RegistrationLoginAppp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package registrationloginappp;
+import java.util.Scanner;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class RegistrationLoginAppp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Register register = new Register();
+        Login login = new Login();
+
+    }
+
+}
